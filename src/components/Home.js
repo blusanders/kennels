2afb92d94495926b68be21fa2f77b6f8b877219f
blusanders <blusanders@yahoo.com>
2021-03-02 15:49:59 -0600

    Auth working. Session var name displaying on home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import { PropsAndState } from './PropsAndState'
 
 export const Home = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <PropsAndState yourName={"Blu"} />
+        <div>
+            <div>
+                <h2>Nashville Kennels</h2>
+                Loving care when you're not there.
+            </div>
+            <div>
+                Hi {localStorage.getItem('kennel_customerName') || ''}
+            </div>
+        </div>
     </>
-)   
+)
+
